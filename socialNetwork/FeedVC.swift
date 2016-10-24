abb212e511c6625566a5a9f1d9d6d7660b4e29df
pdn1905 <phan.ngoc1905@gmail.com>
2016-10-24 23:12:03 +0700

    layout feedvc

--- socialNetwork/FeedVC.swift
+++ socialNetwork/FeedVC.swift
@@ -13,6 +13,7 @@ import SwiftKeychainWrapper
 class FeedVC: UIViewController {
     
     
+    @IBOutlet weak var tableView: UITableView!
     @IBAction func button(_ sender: AnyObject) {
        let keychainResult = KeychainWrapper.removeObjectForKey(UID_KEYCHAIN)
         print("UNINO: ID remove from keychain\(keychainResult)")
@@ -23,6 +24,19 @@ class FeedVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
 
+    }
+}
+
+extension FeedVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
     }
 }
